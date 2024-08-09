e2b91ba6706cea6d0a3ef9676a3340d4152c537e
tarun <tarunrk2002@gmail.com>
2024-08-09 18:18:43 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System.Diagnostics;
+
+namespace enumerables
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            IEnumerable<int> a = abc();
+
+            foreach (int i in a)
+            {
+                Console.WriteLine(i);
+            }
+        }
+        public static IEnumerable<int> abc() 
+        {
+            for (int i = 0; i < 10; i++)
+            {
+               yield return i;
+            }
+            
+        }
+    }
+}
